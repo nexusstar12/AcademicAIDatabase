b290019823e8e3b2611448da92a2aababfea5834
nexusstar12 <47119039+nexusstar12@users.noreply.github.com>
2023-12-06 16:43:13 -0800

    Update unit_tests.py

--- milestones/Milestone2/unit_tests.py
+++ milestones/Milestone2/unit_tests.py
@@ -4,4 +4,118 @@
 # without using the functionality provided by your bot. Once all your tests passed, then you can
 # integrate these functions with your bot logic in main.py
 
+import unittest
+import asyncio
+from unittest.mock import patch, MagicMock
+from main import (
+    student_perf, course_enroll, teacher_review, classroom_util,
+    new_student, add_course, update_grade, reassign_teacher,
+    remove_course, expel_student, set_attendance_alert,
+    set_grade_alert, auto_promote, calc_gpa, recommend_course
+)
 
+class MockContext:
+    def __init__(self):
+        self.message = MagicMock()
+        self.author = MagicMock()
+        self.author.name = "TestUser"
+        self.send = MagicMock()
+
+class TestDiscordBotCommands(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.ctx = MockContext()
+
+    @patch('models.StudentModel.get_average_gpa_by_major')
+    async def test_student_perf(self, mock_get_average_gpa_by_major):
+        mock_get_average_gpa_by_major.return_value = "Average GPA result"
+        await student_perf(self.ctx, "Computer Science", 10)
+        self.ctx.send.assert_called_with("Average GPA result")
+
+    @patch('models.CourseModel.get_enrollment_report')
+    async def test_course_enroll(self, mock_get_enrollment_report):
+        mock_get_enrollment_report.return_value = "Enrollment report result"
+        await course_enroll(self.ctx, 10)
+        self.ctx.send.assert_called_with("Enrollment report result")
+
+    @patch('models.EducatorModel.get_teachers_above_threshold')
+    async def test_teacher_review(self, mock_get_teachers_above_threshold):
+        mock_get_teachers_above_threshold.return_value = "Teacher review result"
+        await teacher_review(self.ctx, 75)
+        self.ctx.send.assert_called_with("Teacher review result")
+
+    @patch('models.ClassroomModel.get_classroom_usage')
+    async def test_classroom_util(self, mock_get_classroom_usage):
+        mock_get_classroom_usage.return_value = "Classroom utilization result"
+        await classroom_util(self.ctx, 5)
+        self.ctx.send.assert_called_with("Classroom utilization result")
+
+    # Continue with similar tests for the rest of the commands
+
+    @patch('models.StudentModel.enroll_new_student')
+    async def test_new_student(self, mock_enroll_new_student):
+        mock_enroll_new_student.return_value = "New student enrolled result"
+        await new_student(self.ctx, "Computer Science")
+        self.ctx.send.assert_called_with("New student enrolled result")
+
+    @patch('models.CourseModel.add_new_course')
+    async def test_add_course(self, mock_add_new_course):
+        mock_add_new_course.return_value = "New course added result"
+        await add_course(self.ctx, "Physics")
+        self.ctx.send.assert_called_with("New course added result")
+
+    @patch('models.GradeModel.update_course_grades')
+    async def test_update_grade(self, mock_update_course_grades):
+        mock_update_course_grades.return_value = "Grades updated result"
+        await update_grade(self.ctx, "CS101", "excellent_attendance")
+        self.ctx.send.assert_called_with("Grades updated result")
+
+    @patch('models.TeacherModel.reassign_teacher')
+    async def test_reassign_teacher(self, mock_reassign_teacher):
+        mock_reassign_teacher.return_value = "Teacher reassigned result"
+        await reassign_teacher(self.ctx, "Math", "Algebra")
+        self.ctx.send.assert_called_with("Teacher reassigned result")
+
+    @patch('models.CourseModel.remove_course')
+    async def test_remove_course(self, mock_remove_course):
+        mock_remove_course.return_value = "Course removed result"
+        await remove_course(self.ctx, "CS101")
+        self.ctx.send.assert_called_with("Course removed result")
+
+    @patch('models.StudentModel.expel_student')
+    async def test_expel_student(self, mock_expel_student):
+        mock_expel_student.return_value = "Student expelled result"
+        await expel_student(self.ctx, "poor_performance")
+        self.ctx.send.assert_called_with("Student expelled result")
+
+    @patch('models.AttendanceModel.set_attendance_alert')
+    async def test_set_attendance_alert(self, mock_set_attendance_alert):
+        mock_set_attendance_alert.return_value = "Attendance alert set result"
+        await set_attendance_alert(self.ctx, 75)
+        self.ctx.send.assert_called_with("Attendance alert set result")
+
+    @patch('models.GradeAlertModel.set_grade_alert')
+    async def test_set_grade_alert(self, mock_set_grade_alert):
+        mock_set_grade_alert.return_value = "Grade alert set result"
+        await set_grade_alert(self.ctx, 5)
+        self.ctx.send.assert_called_with("Grade alert set result")
+
+    @patch('models.StudentPromotionModel.auto_promote')
+    async def test_auto_promote(self, mock_auto_promote):
+        mock_auto_promote.return_value = "Auto promote executed result"
+        await auto_promote(self.ctx, "pass_fail")
+        self.ctx.send.assert_called_with("Auto promote executed result")
+
+    @patch('models.GPAModel.calculate_gpa')
+    async def test_calc_gpa(self, mock_calculate_gpa):
+        mock_calculate_gpa.return_value = "Calculated GPA result"
+        await calc_gpa(self.ctx, "standard")
+        self.ctx.send.assert_called_with("Calculated GPA result")
+
+    @patch('models.CourseRecommendationModel.recommend_courses')
+    async def test_recommend_course(self, mock_recommend_courses):
+        mock_recommend_courses.return_value = "Course recommendations result"
+        await recommend_course(self.ctx, "Science", "A")
+        self.ctx.send.assert_called_with("Course recommendations result")
+
+    if __name__ == '__main__':
+        unittest.main()
