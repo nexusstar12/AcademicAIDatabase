dd4828b9035b33e0c42df0758e5dbc1e94ad5a98
nexusstar12 <47119039+nexusstar12@users.noreply.github.com>
2023-12-06 17:29:49 -0800

    Update unit_tests.py

--- milestones/Milestone2/unit_tests.py
+++ milestones/Milestone2/unit_tests.py
@@ -1,121 +1,168 @@
-# Discord API has a limited number of requests (bot requests) per day.
-# If developers meet that quota, then Discord will put a temporal ban to your bot (24 hours)
-# In order to avoid that, and only for testing, create unit test methods to test your functions
-# without using the functionality provided by your bot. Once all your tests passed, then you can
-# integrate these functions with your bot logic in main.py
+"""
+The code below is just representative of the implementation of a Bot. 
+However, this code was not meant to be compiled as it. It is the responsability 
+of all the students to modifify this code such that it can fit the 
+requirements for this assignments.
+"""
 
-import unittest
-import asyncio
-from unittest.mock import patch, MagicMock
-from main import (
-    student_perf, course_enroll, teacher_review, classroom_util,
-    new_student, add_course, update_grade, reassign_teacher,
-    remove_course, expel_student, set_attendance_alert,
-    set_grade_alert, auto_promote, calc_gpa, recommend_course
+import discord
+import os
+from discord.ext import commands
+from models import *
+from database import Database
+
+TOKEN = os.environ["DISCORD_TOKEN"]
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+  print(f"Bot {bot.user} has joined the room")
+  Database.connect()
+
+
+@bot.command(
+    name="test",
+    description="write your database business requirement for this command here"
 )
+async def _test(ctx, arg1):
+  testModel = TestModel(ctx, arg1)
+  response = testModel.response()
+  await ctx.send(response)
 
-class MockContext:
-    def __init__(self):
-        self.message = MagicMock()
-        self.author = MagicMock()
-        self.author.name = "TestUser"
-        self.send = MagicMock()
 
-class TestDiscordBotCommands(unittest.IsolatedAsyncioTestCase):
-    async def asyncSetUp(self):
-        self.ctx = MockContext()
+# TODO: complete the following tasks:
+#       (1) Replace the commands' names with your own commands
+#       (2) Write the description of your business requirement in the description parameter
+#       (3) Implement your commands' methods.
 
-    @patch('models.StudentModel.get_average_gpa_by_major')
-    async def test_student_perf(self, mock_get_average_gpa_by_major):
-        mock_get_average_gpa_by_major.return_value = "Average GPA result"
-        await student_perf(self.ctx, "Computer Science", 10)
-        self.ctx.send.assert_called_with("Average GPA result")
 
-    @patch('models.CourseModel.get_enrollment_report')
-    async def test_course_enroll(self, mock_get_enrollment_report):
-        mock_get_enrollment_report.return_value = "Enrollment report result"
-        await course_enroll(self.ctx, 10)
-        self.ctx.send.assert_called_with("Enrollment report result")
+@bot.command(name="studentPerf")
+async def student_perf(ctx, major, minimum_students: int):
+  result = StudentModel.get_average_gpa_by_major(major, minimum_students)
+  await ctx.send(result)
 
-    @patch('models.EducatorModel.get_teachers_above_threshold')
-    async def test_teacher_review(self, mock_get_teachers_above_threshold):
-        mock_get_teachers_above_threshold.return_value = "Teacher review result"
-        await teacher_review(self.ctx, 75)
-        self.ctx.send.assert_called_with("Teacher review result")
 
-    @patch('models.ClassroomModel.get_classroom_usage')
-    async def test_classroom_util(self, mock_get_classroom_usage):
-        mock_get_classroom_usage.return_value = "Classroom utilization result"
-        await classroom_util(self.ctx, 5)
-        self.ctx.send.assert_called_with("Classroom utilization result")
+@bot.command(
+    name="courseEnroll",
+    description=
+    "Generate a report showing the number of students enrolled in each course")
+async def course_enroll(ctx, minimum_students: int):
+  response = CourseModel.get_enrollment_report(minimum_students)
+  await ctx.send(response)
 
-    # Continue with similar tests for the rest of the commands
 
-    @patch('models.StudentModel.enroll_new_student')
-    async def test_new_student(self, mock_enroll_new_student):
-        mock_enroll_new_student.return_value = "New student enrolled result"
-        await new_student(self.ctx, "Computer Science")
-        self.ctx.send.assert_called_with("New student enrolled result")
+@bot.command(
+    name="teacherReview",
+    description=
+    "List teachers whose students' average scores are above a certain score threshold"
+)
+async def teacher_review(ctx, score_threshold: int):
+  response = EducatorModel.get_teachers_above_threshold(score_threshold)
+  await ctx.send(response)
 
-    @patch('models.CourseModel.add_new_course')
-    async def test_add_course(self, mock_add_new_course):
-        mock_add_new_course.return_value = "New course added result"
-        await add_course(self.ctx, "Physics")
-        self.ctx.send.assert_called_with("New course added result")
 
-    @patch('models.GradeModel.update_course_grades')
-    async def test_update_grade(self, mock_update_course_grades):
-        mock_update_course_grades.return_value = "Grades updated result"
-        await update_grade(self.ctx, "CS101", "excellent_attendance")
-        self.ctx.send.assert_called_with("Grades updated result")
+@bot.command(
+    name="classroomUtil",
+    description=
+    "Report on the number of classes held in each classroom per week, excluding those used less than a specified number of times"
+)
+async def classroom_util(ctx, minimum_usage: int):
+  response = ClassroomModel.get_classroom_usage(minimum_usage)
+  await ctx.send(response)
 
-    @patch('models.TeacherModel.reassign_teacher')
-    async def test_reassign_teacher(self, mock_reassign_teacher):
-        mock_reassign_teacher.return_value = "Teacher reassigned result"
-        await reassign_teacher(self.ctx, "Math", "Algebra")
-        self.ctx.send.assert_called_with("Teacher reassigned result")
 
-    @patch('models.CourseModel.remove_course')
-    async def test_remove_course(self, mock_remove_course):
-        mock_remove_course.return_value = "Course removed result"
-        await remove_course(self.ctx, "CS101")
-        self.ctx.send.assert_called_with("Course removed result")
+@bot.command(name="newStudent",
+             description=
+             "Enroll a new student and assign mandatory courses based on major"
+             )
+async def new_student(ctx, major):
+  response = StudentModel.enroll_new_student(major)
+  await ctx.send(response)
 
-    @patch('models.StudentModel.expel_student')
-    async def test_expel_student(self, mock_expel_student):
-        mock_expel_student.return_value = "Student expelled result"
-        await expel_student(self.ctx, "poor_performance")
-        self.ctx.send.assert_called_with("Student expelled result")
 
-    @patch('models.AttendanceModel.set_attendance_alert')
-    async def test_set_attendance_alert(self, mock_set_attendance_alert):
-        mock_set_attendance_alert.return_value = "Attendance alert set result"
-        await set_attendance_alert(self.ctx, 75)
-        self.ctx.send.assert_called_with("Attendance alert set result")
+@bot.command(
+    name="addCourse",
+    description="Add a new course to a department and assign a default teacher"
+)
+async def add_course(ctx, department):
+  response = CourseModel.add_new_course(department)
+  await ctx.send(response)
 
-    @patch('models.GradeAlertModel.set_grade_alert')
-    async def test_set_grade_alert(self, mock_set_grade_alert):
-        mock_set_grade_alert.return_value = "Grade alert set result"
-        await set_grade_alert(self.ctx, 5)
-        self.ctx.send.assert_called_with("Grade alert set result")
 
-    @patch('models.StudentPromotionModel.auto_promote')
-    async def test_auto_promote(self, mock_auto_promote):
-        mock_auto_promote.return_value = "Auto promote executed result"
-        await auto_promote(self.ctx, "pass_fail")
-        self.ctx.send.assert_called_with("Auto promote executed result")
+@bot.command(
+    name="updateGrade",
+    description="Update grades for a specific course based on certain criteria"
+)
+async def update_grade(ctx, course_id, criteria):
+  response = GradeModel.update_course_grades(course_id, criteria)
+  await ctx.send(response)
 
-    @patch('models.GPAModel.calculate_gpa')
-    async def test_calc_gpa(self, mock_calculate_gpa):
-        mock_calculate_gpa.return_value = "Calculated GPA result"
-        await calc_gpa(self.ctx, "standard")
-        self.ctx.send.assert_called_with("Calculated GPA result")
 
-    @patch('models.CourseRecommendationModel.recommend_courses')
-    async def test_recommend_course(self, mock_recommend_courses):
-        mock_recommend_courses.return_value = "Course recommendations result"
-        await recommend_course(self.ctx, "Science", "A")
-        self.ctx.send.assert_called_with("Course recommendations result")
+@bot.command(
+    name="reassignTeacher",
+    description=
+    "Reassign a teacher to a different course based on departmental needs and teacher's expertise"
+)
+async def reassign_teacher(ctx, needs, expertise):
+  result = TeacherModel.reassign_teacher(needs, expertise)
+  await ctx.send(result)
 
-    if __name__ == '__main__':
-        unittest.main()
+
+@bot.command(name="removeCourse",
+             description="Remove a course if no students are enrolled")
+async def remove_course(ctx, course_code):
+  result = CourseModel.remove_course(course_code)
+  await ctx.send(result)
+
+
+@bot.command(name="expelStudent",
+             description="Expel a student based on specific criteria")
+async def expel_student(ctx, criteria):
+  result = StudentModel.expel_student(criteria)
+  await ctx.send(result)
+
+
+@bot.command(name="setAttendanceAlert",
+             description=
+             "Set an alert for when attendance drops below a certain threshold"
+             )
+async def set_attendance_alert(ctx, threshold: float):
+  result = AttendanceModel.set_attendance_alert(threshold)
+  await ctx.send(result)
+
+
+@bot.command(name="setGradeAlert",
+             description="Set a notification alert for grade changes")
+async def set_grade_alert(ctx, change_threshold: float):
+  result = GradeAlertModel.set_grade_alert(change_threshold)
+  await ctx.send(result)
+
+
+@bot.command(name="autoPromote",
+             description="Automatically promote students based on criteria")
+async def auto_promote(ctx, criteria):
+  result = StudentPromotionModel.auto_promote(criteria)
+  await ctx.send(result)
+
+
+@bot.command(name="calcGPA",
+             description="Calculate GPA based on specific weightings")
+async def calc_gpa(ctx, weightings):
+  result = GPAModel.calculate_gpa(weightings)
+  await ctx.send(result)
+
+
+@bot.command(name="recommendCourse",
+             description="Recommend courses based on interests and performance"
+             )
+async def recommend_course(ctx, interests, performance):
+  recommendations = CourseRecommendationModel.recommend_courses(
+      interests, performance)
+  await ctx.send(recommendations)
+
+
+bot.run(TOKEN)
